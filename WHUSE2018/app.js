0bff71967373af4007346a26412748c1c5227c4a
Yao Li <921106097@qq.com>
2018-03-23 15:16:40 +0800

    add user select/insert/update/delete api

--- WHUSE2018/app.js
+++ WHUSE2018/app.js
@@ -1,3 +1,5 @@
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,6 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var userInfoRouter = require('./routes/user');
 
 var app = express();
 
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/user',userInfoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
