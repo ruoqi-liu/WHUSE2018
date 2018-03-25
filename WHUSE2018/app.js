8add842e861131705d80a4dd4b79b4407b5657e8
Yao Li <921106097@qq.com>
2018-03-25 14:52:51 +0800

    finish whucs crawler

--- WHUSE2018/app.js
+++ WHUSE2018/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/user',userInfoRouter);
+app.use('/api/user',userInfoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
