ca9def9b34616a8f030fd1ba161fe91bccf3c3f3
Yao Li <921106097@qq.com>
2018-04-03 14:50:11 +0800

    add RESTful standard

--- WHUSE2018/routes/Passport/Passport.js
+++ WHUSE2018/routes/Passport/Passport.js
@@ -5,7 +5,7 @@ var monk = require('monk');
 var db = monk('localhost:27017/WHUSE');
 var collection = db.get('user');
 
-passport.serializeUser(function(user,done) {//Ð´Èësession,done ¾ÍÊÇÒ»¸öÖÐ¼ä¼þ£¬½ÓÊÜË³ÐòÎªerr,user,info²¢½«ËûÃÇÐ´Èësession
+passport.serializeUser(function (user, done) {//Ð´Èësession,done ¾ÍÊÇÒ»¸öÖÐ¼ä¼þ£¬½ÓÊÜË³ÐòÎªerr,user,info²¢½«ËûÃÇÐ´Èësession
     console.log('serializing.');
     done(null, user);
 }
@@ -13,27 +13,27 @@ passport.serializeUser(function(user,done) {//Ð´Èësession,done ¾ÍÊÇÒ»¸öÖÐ¼ä¼þ£¬½
 
 passport.deserializeUser(function (user, done) {//Ð´Èësession
     console.log('deseralizing');
-    done(null,user);
+    done(null, user);
 }
 );
 
-passport.use('logIn',new LocalStrategy(
+passport.use('logIn', new LocalStrategy(
     {
         usernameField: 'name',
         passwordField: 'password',
-        passReqToCallback:false
+        passReqToCallback: false
     }, function (username, password, done) {//ÉèÖÃÎªtrueÖ®ºóµÚÒ»¸ö²ÎÊý»á½«req´«Èë    
         if (!username || !password)
             done(null, false, { 'valid': '0', message: 'username or password cannot be null.' });
-        collection.find({ name: username, password: password }, {fields:{ name:1,password:1}}, function (err, result) {
+        collection.find({ name: username, password: password }, { fields: { name: 1, password: 1,image: 1 } }, function (err, result) {
             if (err) throw err;
             console.log(result);
             if (result)
                 if (result.length == 1) {
-                   return done(null, { 'username': username, 'valid': '1' });
+                    return done(null, { 'username': username,'photo':result.image ,'valid': '1' });
                 }
                 else
-                    return done(null, false, {'valid':'0',message:'username or password incorrect.'});
+                    return done(null, false, { 'valid': '0', message: 'username or password incorrect.' });
         });
         return;
     }
@@ -44,15 +44,17 @@ passport.use('signUp', new LocalStrategy(
         usernameField: 'name',
         passwordField: 'password',
         passReqToCallback: true
-    }, function (req,username, password, done) {
-        collection.insert({ name: username, password:password}, function (err, result) {
+    }, function (req, username, password, done) {
+        if (!username || !password)
+            done(null, false, { 'valid': '0', message: 'username or password cannot be null.' });
+        collection.insert({ name: username, password: password }, function (err, result) {
             if (err)
                 if (err.code == 11000)
-                    return done(null, false, { message: 'duplicated username','valid':'0' });
+                    return done(null, false, { message: 'duplicated username', 'valid': '0' });
                 else
-                    return done(null, false, { message: 'unknown error','valid':'0' });
+                    return done(null, false, { message: 'unknown error', 'valid': '0' });
             else
-                return done(null, {'username':username,valid:'1'});
+                return done(null, { 'username': username, 'photo': result.image, valid: '1' });
 
             return;
         });
@@ -62,11 +64,18 @@ passport.use('signUp', new LocalStrategy(
 
 var isAuthenticated = function (req, res, next) {
     if (req.isAuthenticated()) return next();
+    res.send({ 'tologin': '1' });
+    //res.setHeader ('method', 'GET' );
+    //res.redirect('/users');
+    return;
+}
 
-    res.setHeader ('method', 'GET' );
-    res.redirect('/users');
+var userLogout = function (req, res, next) {
+    if (req.isAuthenticated()) req.logout();
+    res.send({ 'islogout': '1' });
     return;
 }
 
 exports.authentic = isAuthenticated;
 exports.passport = passport;
+exports.logout = userLogout;
--- WHUSE2018/routes/user.js
+++ WHUSE2018/routes/user.js
@@ -5,16 +5,17 @@ var flash = require('connect-flash');
 var router = express.Router();
 var passport = require('./Passport/Passport').passport;
 var isAuthentic = require('./Passport/Passport').authentic;
+var logout = require('./Passport/Passport').logout;
 
 var monk = require('monk');
 var db = monk('localhost:27017/WHUSE');
 var collection = db.get('user');
 
 router.use(session({
-    resave: true,  
-    saveUninitialized: true ,
+    resave: true,
+    saveUninitialized: true,
     secret: 'meow',
-    rolling:true,
+    rolling: true,
     cookie: { maxAge: 60000000 }
 }
 )
@@ -23,7 +24,7 @@ router.use(session({
 router.use(passport.initialize());
 router.use(passport.session());
 router.use(flash());
- 
+
 router.post('/login', function (req, res, next) {
     passport.authenticate('logIn',
         function (err, user, info) {//user and info are passed by the PassportChain not in the session
@@ -32,17 +33,17 @@ router.post('/login', function (req, res, next) {
             if (!user) {
                 req.flash('error', { msg: info.message });
                 req.logout();
-                res.send(info);
+                res.send({'islogin':'0','message':info.message});
                 return;
             }
             else
-            req.logIn(user, function (err) {
+                req.logIn(user, function (err) {
 
-                if (err) return next(err);
-                res.send({ valid: '1', 'username': user.username });
-            });
+                    if (err) return next(err);
+                    res.send({ 'islogin': '1', 'username': user.username });
+                });
             return;
-        })(req,res,next);
+        })(req, res, next);
 }
 );
 
@@ -60,68 +61,71 @@ router.post('/login', function (req, res, next) {
 //}
 
 
-router.post('/', function (req, res, next) {//add
+router.post('/register', function (req, res, next) {//add
     passport.authenticate('signUp', function (err, user, info) {
         if (err) next(err);
 
         if (!user) {
-            req.flash('error', {msg:info.message});
+            req.flash('error', { msg: info.message });
             req.logout();
-            res.send(info);
+            res.send({ 'isregister':'0','message':info.message });
             return;
         }
         else
             req.logIn(user, function (err) {
 
                 if (err) return next(err);
-                res.send({ valid: '1', 'username': user.username });
+                res.send({ 'isregister': '1', 'username': user.username });
             });
         return;
     })(req, res, next);
-    //collection.insert({ name: req.body.name, password: req.body.password }, function (err, result) {
-    //    if (err)
-    //        if (err.code == 11000)
-    //            res.send({ 'succeed': '0', 'error': 'duplicated name' });
-    //        else
-    //            res.send({ 'succeed': '0', 'error': 'unkonwn' });
-    //    else
-    //        res.send({ succeed: '1' })
-
-    //    return;
-    //});
-    //return;
 });
 
-router.put('/', isAuthentic, function (req, res, next) {//update
+//collection.insert({ name: req.body.name, password: req.body.password }, function (err, result) {
+//    if (err)
+//        if (err.code == 11000)
+//            res.send({ 'succeed': '0', 'error': 'duplicated name' });
+//        else
+//            res.send({ 'succeed': '0', 'error': 'unkonwn' });
+//    else
+//        res.send({ succeed: '1' })
+
+//    return;
+//});
+//return;
+
+router.put('/:name', isAuthentic, function (req, res, next) {//update
     if (!req.body.content) return;
-    console.log(req.body.content);
-    collection.update({ name: req.body.name, password: req.body.password }, req.body.content, function (err, result) {
+    console.log(req.params.name);
+    collection.update({ name: req.params.name, password: req.body.password }, req.body.content, function (err, result) {
         if (err)
             if (err.code == 11000)
-                res.send({ succeed: '0', error: 'duplicated name' });
+                res.send({ 'isupdate': '0', 'message': 'duplicated name' });
             else
-                res.send({ succeed: '0', error: 'unkonwn' });
+                res.send({ 'isupdate': '0', 'message': 'unkonwn' });
         else if (result.n > 0)
-            res.send({ succeed: '1' });
+            res.send({ 'isupdate': '1' });
         else
-            res.send({ succeed: '0', error: 'invalid password' });
+            res.send({ 'isupdate': '0', 'message': 'invalid password' });
         return;
     }
     );
     return;
 });
 
-router.delete('/', isAuthentic,function (req, res, next) {//delete
-    collection.remove({ name: req.body.name, password: req.body.password }, function (err, result) {
+router.delete('/:name', isAuthentic, function (req, res, next) {//delete
+    collection.remove({ name: req.params.name, password: req.body.password }, function (err, result) {
         if (err) {
-            res.send({ succeed: '0', error: 'unknown' });
+            res.send({ 'isdelete': '0', 'message': 'unknown' });
             return;
         }
 
-        res.send({ succeed: '1' });
+        res.send({ 'isdelete': '1' });
     });
     return
 });
 
+router.use('/logout', logout
+ );
 
 module.exports = router;
