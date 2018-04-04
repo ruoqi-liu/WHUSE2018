810f12f2df39190dc336f61066afd25bcd4ecfe7
Yao Li <921106097@qq.com>
2018-04-04 23:43:27 +0800

    add node-segment

---
+++ WHUSE2018/Segment/mySegment.js
@@ -0,0 +1,16 @@
+﻿var Segment = require('segment');
+
+var segment = new Segment();
+segment.useDefault();
+segment.loadStopwordDict('stopword.txt');
+
+var getDefaultSegment = function (doc) {
+    console.log(doc);
+    return segment.doSegment(doc, {
+        simple: true,
+        stripPunctuation: true,
+        stripStopword:true
+    });
+};
+
+console.log(getDefaultSegment('zhe时一段中文的ceshi，sad.//././.this 是 a test of English'));
