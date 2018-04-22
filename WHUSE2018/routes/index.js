87ba58fb86e3da571dd7c1a141fdba9634b820d2
zhw <szzhw3166@163.com>
2018-04-22 23:53:00 +0800

    首页及登录注册跳转 1.0

--- WHUSE2018/routes/index.js
+++ WHUSE2018/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/visit', function (req, res, next) {
+    res.render('visit', { title: 'After login' });
+});
 module.exports = router;
