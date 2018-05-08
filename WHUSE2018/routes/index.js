b57757a5b0436a163b7f0924f9058c9de1df1977
Maxchloe <1025933456@qq.com>
2018-05-08 19:46:49 +0800

    maybe finish register and login

--- WHUSE2018/routes/index.js
+++ WHUSE2018/routes/index.js
@@ -1,25 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
     var user = req.user;
     if(!user) user={username:'用户'};
-    res.render('index', { title: 'Express' ,'username':user.username});
+    res.render('index', { title: 'Index' ,'username':user.username});
 });
 
 router.get('/index', function (req, res, next) {
     var user = req.user;
     if(!user) user={username:'用户'};
-  res.render('index', { title: 'Express','username':user.username });
+  res.render('index', { title: 'Index','username':user.username });
+});
+
+router.get('/user', function (req, res, next) {
+    var user = req.user;
+    if(!user) user={username:'用户'};
+    res.render('user', { title: 'User' ,'username':user.username});
 });
 
 router.get('/lost', function (req, res, next) {
-    /*var type = req.params.type;
-    var page = req.params.page;*/
     var user = req.user;
     if(!user) user={username:'用户'};
-    res.render('lost', { title: 'Lost and Found' ,/*type:type, page:page,*/ 'username':user.username});
+    res.render('lost', { title: 'Lost and Found' ,'username':user.username});
 });
 
 router.get('/register/:source/:type', function (req, res, next) {
